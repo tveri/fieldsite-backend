26f4ed1c70498efbded9a57ec1d52b68936047b0
tveri <rotro.volodya.tve@gmail.com>
2022-10-01 17:53:08 +0300

    new table

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, request, send_from_directory, jsonify, render_template,
 import re, os, shutil, datetime, time, json, sqlite3
 import openpyxl as ox
 from flask_cors import CORS, cross_origin
+from openpyxl.utils.cell import get_column_letter
 
 MONTHS = [
     'Январь',
@@ -26,11 +27,14 @@ app.config["HOST"] = "0.0.0.0"
 application = app
 CORS(app, support_credentials=True)
 
-db = sqlite3.connect('./fields.db', check_same_thread=False)
+db = sqlite3.connect('./allfields.db', check_same_thread=False)
 cur = db.cursor()
 
 fieldColumns = {col[0]: col[1] for col in cur.execute('PRAGMA table_info(field62z05)').fetchall()}
+fieldColumnsNames = {fieldColumns[k]: k for k in fieldColumns}
 
+fieldGlobalColumns = {col[0]: col[1] for col in cur.execute('PRAGMA table_info(field62z05global)').fetchall()}
+fieldGlobalColumnsNames = {fieldGlobalColumns[k]: k-1 for k in fieldGlobalColumns}
 
 
 def getConnection():
@@ -45,6 +49,12 @@ def tryDecorator(func):
             print(e)
             return 0
     return wrapper
+
+
+def Cfunc(data, row, globalData):
+    if data[row][1] < globalData[fieldGlobalColumnsNames['sowing_date']]:
+        return 0
+    return (data[row][1] - globalData[fieldGlobalColumnsNames['sowing_date']] + 86400) // 86400
 
 
 def Gfunc(data, row):
@@ -83,19 +93,19 @@ def Mfunc(data, row):
 
 
 def Nfunc(data, row, globalData):
-    return globalData[0] * globalData[1] * data[row][12] * 1000
+    return globalData[fieldGlobalColumnsNames['porisity']] * globalData[fieldGlobalColumnsNames['ppv']] * data[row][12] * 1000
 
 
 def Ofunc(data, row, globalData):
-    return globalData[0] * globalData[1] * globalData[2] * data[row][12] * 1000
+    return globalData[fieldGlobalColumnsNames['porisity']] * globalData[fieldGlobalColumnsNames['ppv']] * globalData[fieldGlobalColumnsNames['pres_ppv']] * data[row][12] * 1000
 
 
 def Pfunc(data, row, globalData):
-    return data[row][13] * globalData[3] - data[row][14]
+    return data[row][13] * globalData[fieldGlobalColumnsNames['max_ppv']] - data[row][14]
 
 
 def Qfunc(data, row, globalData):
-    return data[row][13] * globalData[4]
+    return data[row][13] * globalData[fieldGlobalColumnsNames['start_ppv']]
 
 
 def Rfunc(data, row):
@@ -112,7 +122,7 @@ def Sfunc(data, row):
 def Tfunc(data, row):
     return data[row][13] - data[row][18] + data[row][11]
 
-
+@tryDecorator
 def Wfunc(data, row):
     if data[row][20] + data[row][21] < data[row][19]:
         return data[row][20] + data[row][21]
@@ -148,8 +158,9 @@ def ACfunc(data, row):
     return -(data[row][13] + data[row][17] - data[row][25])
 
 
-def calcAllData(data, globalData):
+def calcAllData(data, globalData, timestamp=False):
     for row in range(len(data)):
+        data[row][2] = Cfunc(data, row, globalData)
         data[row][6] = round(Gfunc(data, row), 2)
         data[row][7] = round(Hfunc(data, row), 2)
         data[row][8] = round(Ifunc(data, row), 2)
@@ -170,6 +181,7 @@ def calcAllData(data, globalData):
         data[row][26] = round(AAfunc(data, row), 2)
         data[row][27] = round(ABfunc(data, row), 2)
         data[row][28] = round(ACfunc(data, row), 2)
+        data[row][1] = data[row][1] if timestamp else dateToStr(datetime.datetime.fromtimestamp(data[row][1]))
     return data
 
 def dataFromDBtoTableData(rawData, timestamp=False):
@@ -179,14 +191,14 @@ def dataFromDBtoTableData(rawData, timestamp=False):
         data.append(columnsInRow[:])
         date = datetime.datetime.fromtimestamp(int(d) if (d := rawData[row][1]) else 0)
         data[row][0] = round(d if (d := rawData[row][0]) else 0, 2)
-        data[row][1] = f'{date.day if date.day > 9 else f"0{date.day}"}.{date.month if date.month > 9 else f"0{date.month}"}.{date.year}' if not timestamp else int(d) if (d := rawData[row][1]) else 0
-        data[row][2] = round(d if (d := rawData[row][2]) else 0, 2)
-        data[row][3] = round(d if (d := rawData[row][3]) else 0, 2)
-        data[row][4] = round(d if (d := rawData[row][4]) else 0, 2)
-        data[row][5] = round(d if (d := rawData[row][5]) else 0, 2)
-        data[row][10] = round(d if (d := rawData[row][6]) else 0, 2)
-        data[row][20] = round(d if (d := rawData[row][7]) else 0, 2)
-        data[row][21] = round(d if (d := rawData[row][8]) else 0, 2)
+        data[row][1] = int(d) if (d := rawData[row][1]) else 0
+        # data[row][2] = round(d if (d := rawData[row][2]) else 0, 2)
+        data[row][3] = round(d if (d := rawData[row][2]) else 0, 2)
+        data[row][4] = round(d if (d := rawData[row][3]) else 0, 2)
+        data[row][5] = round(d if (d := rawData[row][4]) else 0, 2)
+        data[row][10] = round(d if (d := rawData[row][5]) else 0, 2)
+        data[row][20] = round(d if (d := rawData[row][6]) else 0, 2)
+        data[row][21] = round(d if (d := rawData[row][7]) else 0, 2) if type(rawData[row][7]) == float else rawData[row][7]
     return data
 
 def getDataFromDB(db, field='62-05'):
@@ -197,8 +209,16 @@ def getGlobalDataFromDB(db, field='62-05'):
     return cur.execute(f"SELECT * FROM field{field.replace('-', 'z')}global").fetchall()[0][1:]
 
 
+def getFieldsFromDB(db):
+    return cur.execute(f"SELECT * FROM fields").fetchall()
+
+
 def getTableData(db, field='62-05', timestamp=False):
-    return calcAllData(dataFromDBtoTableData(getDataFromDB(db, field), timestamp=timestamp), getGlobalDataFromDB(db, field))
+    return calcAllData(dataFromDBtoTableData(getDataFromDB(db, field), timestamp=timestamp), getGlobalDataFromDB(db, field), timestamp=timestamp)
+
+
+def dateToStr(date):
+    return f'{date.day if date.day > 9 else f"0{date.day}"}.{date.month if date.month > 9 else f"0{date.month}"}.{date.year}'
 
 
 @app.route('/api/getgraphics/')
@@ -217,13 +237,75 @@ def getGraphics(field='62-05'):
                 })
 
 
-    return make_response(resp)
+    return make_response(jsonify(resp))
 
 
 @app.route('/api/gettable/')
 @cross_origin(supports_credentials=True)
 def getTable(field='62-05'):
     return make_response(jsonify(getTableData(db, field)))
+
+
+@app.route('/api/getsettingstable/')
+@cross_origin(supports_credentials=True)
+def getSettingsTable(field='62-05'):
+    data = [[d] for d in getGlobalDataFromDB(db, field)]
+    return make_response(jsonify(data))
+
+
+@app.route('/api/getdashboardtable/')
+@cross_origin(supports_credentials=True)
+def getDashboardTable():
+    resp = {
+        'header': [
+            [],
+            ['','','','','','','','','','',''],
+            ['№', '', 'Номер поля', 'Дата сева', 'Дни', 'Pivot №', 'Площадь, Га', 'мм', '% FC', 'мм', 'Дни полива', 'irrigation', 'rainfall']
+        ],
+        'tables': [
+            []
+        ] 
+    }
+    data = getDataFromDB(db, '62-05')
+    fields = getFieldsFromDB(db)
+
+    lastMonth = -1
+    for d in data:
+        if lastMonth != (month := datetime.datetime.fromtimestamp(d[fieldColumnsNames['date']]).month):
+            resp['header'][1].append(MONTHS[month-1])
+            lastMonth = month
+            continue
+        resp['header'][1].append('')
+    
+    resp['header'][1].append('')
+    resp['header'][1].append('')
+
+    resp['header'][2] = resp['header'][2][:11] + [datetime.datetime.fromtimestamp(d[fieldColumnsNames['date']]).day for d in data] + resp['header'][2][11:]
+
+    for i in range(len(resp['header'][2])):
+        resp['header'][0].append(get_column_letter(i+1))
+
+    rowTemplate = ['' for _ in range(len(resp['header'][2]))]
+    for i, field in fields:
+        date = 1663693220
+        data = getTableData(db, field, timestamp=True)
+        dataGlobal = getGlobalDataFromDB(db, field)
+
+        resp['tables'][0].append(rowTemplate[:])
+        resp['tables'][0][i][0] = i+1
+        resp['tables'][0][i][1] = field
+        resp['tables'][0][i][2] = field
+        resp['tables'][0][i][3] = dateToStr(datetime.datetime.fromtimestamp(dataGlobal[fieldGlobalColumnsNames['sowing_date']]))
+        resp['tables'][0][i][4] = (datetime.datetime.now().timestamp() - dataGlobal[fieldGlobalColumnsNames['sowing_date']]) // 86400
+        resp['tables'][0][i][8] = [d[13] for d in data if d[1]//86400*86400 == date//86400*86400][0]
+        resp['tables'][0][i][10] = sum([1 for d in data if (d[21] if isinstance(d[21], (int, float)) else 0) > 0])
+        for col, val in enumerate([round((d[20] if isinstance(d[20], (int, float)) else 0) + (d[21] if isinstance(d[21], (int, float)) else 0), 2) for d in data]):
+            resp['tables'][0][i][col+11] = val
+        resp['tables'][0][i][-2] = round(sum([(d[21] if isinstance(d[21], (int, float)) else 0) for d in data]), 2)
+        resp['tables'][0][i][-1] = round(sum([(d[20] if isinstance(d[20], (int, float)) else 0) for d in data]), 2)
+    
+    return make_response(jsonify(resp))
+
 
 # data = [(r[0].value, int(datetime.datetime.timestamp(r[1].value)), r[2].value, r[3].value, r[4].value, r[5].value, r[10].value, r[20].value, r[21].value) for r in [row for row in ws.rows][5:] if r[1].value]
 
